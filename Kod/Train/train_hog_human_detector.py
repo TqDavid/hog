aacc127ea0f5f3079765265fc40e36509431a889
Igor Smolkovič <igor.smolkovic2@gmail.com>
2013-12-27 04:18:08 +0100

    dodan train_auto

--- Kod/Train/train_hog_human_detector.py
+++ Kod/Train/train_hog_human_detector.py
@@ -7,44 +7,40 @@ from Extract import HOG, Extract
 import numpy as np
 
 
-db = LoadDataSet("../../..//INRIAPerson")
+db = LoadDataSet("../../../INRIAPerson")
 cut = CutPicture(64.0, 128.0, 3)
 hog = HOG(9, (6,6), (3,3))
 ex = Extract(hog, cut)
 
 
-# Ovaj dio radi ucenje
 pos, neg = db.loadTrainSet()
-samples, classes = ex.getSamples(pos, neg)
+X, y = ex.getSamples(pos, neg)
 
-samples = np.array(samples).astype('float32')
-classes = np.array(classes).astype('float32')
+X = np.array(X).astype('float32')
+y = np.array(y).astype('float32')
 
 print 'Ucenje'
-model = cv2.SVM()
-model.train(samples, classes)
-#model.save('Model/model1.svm')
+model = cv2.SVM() 
+params = dict(kernel_type=cv2.SVM_LINEAR, svm_type=cv2.SVM_C_SVC) 
+model.train_auto(X, y, None, None, params, 3) #kfold=3 (default: 10)
 
+model.save('Model/model.svm')
 
-# ovaj dio radi testiranje 
-
-#model = cv2.SVM()
-#model.load('Model/model.svm')
 
 print 'Testiranje'
 pos = db.loadTestSet()
-samples = ex.getTestSamples(pos)
+X = ex.getTestSamples(pos)
 
 true = 0
 false = 0
-N = len(samples)
+N = len(X)
 
-for s in samples:
-	if int(model.predict(np.array(s).astype('float32'))) == 1:
+for sample in X:
+	if int(model.predict(np.array(sample).astype('float32'))) == 1:
 		true = true + 1
 	else:
 		false = false + 1
 
 print 'N: ', N
-print 'True: ', true
+print 'True: ', true, true/float(N)
 print 'False ', false
