1d3db7be0c6b2d52676a7ae4ddcfb7b1ca4ec979
Igor Smolkovič <igor.smolkovic2@gmail.com>
2014-01-24 20:32:24 +0100

    Izmjena

--- Kod/Train/train_hog_human_detector.py
+++ Kod/Train/train_hog_human_detector.py
@@ -12,7 +12,7 @@ cut = CutPicture(64.0, 128.0, 3)
 hog = HOG(9, (6,6), (3,3))
 ex = Extract(hog, cut)
 
-
+'''
 print 'Generiranje znacajki...'
 pos, neg = db.loadTrainSet()
 X, y = ex.getSamples(pos, neg)
@@ -26,15 +26,29 @@ params = dict(kernel_type=cv2.SVM_LINEAR, svm_type=cv2.SVM_C_SVC)
 model.train_auto(X, y, None, None, params, 3) #kfold=3 (default: 10)
 
 model.save('../Model/model.svm')
-
+'''
 print 'Testiranje...'
 model = cv2.SVM()
-#model.load('../Model/model.svm')
+model.load('../Model/model.svm')
 pos = db.loadTestSet()
 X = ex.getTestSamples(pos)
 X = np.array(X).astype('float32')
 
 
+true = 0
+false = 0
+
+for sample in X:
+	if int(model.predict(sample)) == 1:
+		true += 1
+	else:
+		false += 1
+
+print 'N: ', true + false
+print 'True: ', true, true / float(true + false)
+print 'False: ', false
+	
+'''
 ret = model.predict_all( X )
 N = ret.size
 true = np.nonzero( ret )[0].size
@@ -42,4 +56,5 @@ true = np.nonzero( ret )[0].size
 print 'N: ', ret.size
 print 'True: ', true, true / float( N )
 print 'False: ', N - true
+'''
 print 'Gotovo...'
