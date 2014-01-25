1354679bcc592be020a008a34d473001a95fbd92
dsantl <dsantl.ck@gmail.com>
2014-01-25 01:26:17 +0100

    Popravljen train

--- Kod/Train/train_hog_human_detector.py
+++ Kod/Train/train_hog_human_detector.py
@@ -5,9 +5,9 @@ from Cut import CutPicture
 from Extract import HOG, Extract
 
 import numpy as np
+import sys
 
-
-db = LoadDataSet("../../../INRIAPerson")
+db = LoadDataSet(sys.argv[1])
 cut = CutPicture(64.0, 128.0, 3)
 hog = HOG(9, (6,6), (3,3))
 ex = Extract(hog, cut)
@@ -20,26 +20,52 @@ X, y = ex.getSamples(pos, neg)
 X = np.array(X).astype('float32')
 y = np.array(y).astype('float32')
 
+print "Train pos: " + str(len(pos))
+print "Train neg: " + str(len(neg))
+
 print 'Ucenje...'
 model = cv2.SVM() 
 params = dict(kernel_type=cv2.SVM_LINEAR, svm_type=cv2.SVM_C_SVC) 
-model.train_auto(X, y, None, None, params, 3) #kfold=3 (default: 10)
+#model.train_auto(X, y, None, None, params, 3) #kfold=3 (default: 10)
 
-model.save('../Model/model.svm')
+#model.save('../Model/model.svm')
 
 print 'Testiranje...'
 model = cv2.SVM()
-#model.load('../Model/model.svm')
-pos = db.loadTestSet()
-X = ex.getTestSamples(pos)
+model.load('../Model/model.svm')
+
+print "Model je ucitan..."
+
+pos, neg = db.loadTestSet()
+X, y = ex.getSamples(pos, neg)
 X = np.array(X).astype('float32')
+y = np.array(y).astype('float32')
 
+print "Test pos: " + str(len(pos))
+print "Test neg: " + str(len(neg))
 
-ret = model.predict_all( X )
-N = ret.size
-true = np.nonzero( ret )[0].size
+TP = 0
+TF = 0
+FP = 0
+FN = 0
+for i in range(len(X)):
+	ret = int(model.predict(X[i].astype('float32')))
+	if ret == 1 and y[i] == 1:
+		TP += 1
+	elif ret == 0 and y[i] == 0:
+		TF += 1
+	elif ret == 1 and y[i] == 0:
+		FP += 1
+	elif ret == 0 and y[i] == 1:
+		FN += 1
 
-print 'N: ', ret.size
-print 'True: ', true, true / float( N )
-print 'False: ', N - true
-print 'Gotovo...'
+N = len(X)
+print 'N: ', N
+print 'Accuracy: ', (TP+TF) / float( N )
+print 'True: ', (TP+TF)
+print 'False: ', (N-TP-TF)
+print "TP: ", TP
+print "TF: ", TF
+print "FP: ", FP
+print "FN: ", FN
+print 'END...'
